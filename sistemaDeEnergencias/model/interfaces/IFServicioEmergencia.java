acf6c849e3a3777341d593587f110293c7876660
Yojan Giraldo Muñoz <yojangiraldo2020@gmail.com>
2025-01-19 17:03:24 -0500

    save

--- sistemaDeEnergencias/model/interfaces/IFServicioEmergencia.java
+++ sistemaDeEnergencias/model/interfaces/IFServicioEmergencia.java
@@ -1,5 +1,26 @@
 package model.interfaces;
 
+import model.Emergencia;
+
 public interface IFServicioEmergencia {
+
+    String getId();
+
+    int getPersonalDisponible();
+
+    int getCombustible();
+
+    boolean estaDisponible();
+
+    void asignarPersonal(int cantidad);
+
+    void libreraPersonal(int cantidad);
+
+    void gastarCombustible(int cantidad);
+
+    void tanquearCombustible(int cantidad);
+
+    void atenderEmergencia(Emergencia emergencia);
+
 
 }
