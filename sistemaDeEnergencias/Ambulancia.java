c5585798ec3e19c3287ecf613282a8b1b1a84015
Yojan Giraldo Muñoz <yojangiraldo2020@gmail.com>
2025-01-19 14:18:11 -0500

    Configuracion ambulancia

--- sistemaDeEnergencias/Ambulancia.java
+++ sistemaDeEnergencias/Ambulancia.java
@@ -1,3 +1,51 @@
-public class Ambulancia {
+public class Ambulancia implements Responder{
+    private int recursosDisponibles; // Número de ambulancias disponibles
+    private String ubicacion; // Ubicación actual de la estación de bomberos
 
+    // Constructor
+    public Ambulancia(int recursosDisponibles, String ubicacion) {
+        this.recursosDisponibles = recursosDisponibles;
+        this.ubicacion = ubicacion;
+    }
+    @Override
+    public void atenderEmergencia(Emergencia emergencia) {
+        if ("Accidente_vehicular".equalsIgnoreCase(emergencia.getTipoEmergencia())) {
+            if (recursosDisponibles > 0) {
+                System.out.println("Ambulancia atendiendo un " + emergencia.getTipoEmergencia() + " en " + emergencia.getDireccion());
+                System.out.println("Gravedad: " + emergencia.getGravidad());
+                recursosDisponibles--; // Reducir recursos después de asignarlos
+            } else {
+                System.out.println("No hay recursos disponibles para atender esta emergencia.");
+            }
+        } else {
+            System.out.println("Esta emergencia no es relevante para las ambulancias.");
+        }
+    }
+    @Override
+    public void evaluarEstado(Emergencia emergencia) {
+        System.out.println("Estado de recursos disponibles: " + recursosDisponibles);
+        if (recursosDisponibles > 0) {
+            System.out.println("Ambulancias disponibles para atender emergencias.");
+        } else {
+            System.out.println("Ambulancias no disponibles en este momento.");
+        }
+        
+    }
+
+     // Métodos adicionales
+     public int getRecursosDisponibles() {
+        return recursosDisponibles;
+    }
+
+    public void setRecursosDisponibles(int recursosDisponibles) {
+        this.recursosDisponibles = recursosDisponibles;
+    }
+
+    public String getUbicacion() {
+        return ubicacion;
+    }
+
+    public void setUbicacion(String ubicacion) {
+        this.ubicacion = ubicacion;
+    }
 }
